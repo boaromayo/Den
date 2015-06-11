1ecf3da6d41f6cc7419c81e4bf4de7956e3f10b3
boaromayo <nly_poblet@yahoo.com>
2015-06-10 22:56:03 -0700

    Create SceneStatus.java

---
+++ src/scene/SceneStatus.java
@@ -0,0 +1,28 @@
+package scene;
+
+import java.awt.*;
+
+import den.DenInput;
+
+public class SceneStatus extends Scene {
+
+	public SceneStatus() {
+		super();
+	}
+
+	@Override
+	public void init() {
+		
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		
+	}
+	
+	@Override
+	public void updateInput() {
+		
+	}
+
+}
