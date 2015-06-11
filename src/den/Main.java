7940f7b30c2b3fd26906ed71f0d2187979e7f29c
boaromayo <nly_poblet@yahoo.com>
2015-06-10 21:55:47 -0700

    Created Main.java

---
+++ src/den/Main.java
@@ -0,0 +1,47 @@
+/**====================================
+/* THE EPIC OF DEN.
+/* ------------------------------------
+/* code by Nico Poblete
+/* Copyright 2014 OM Independent
+/**====================================**/
+package den;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Main {
+	/**=======================================
+	/* MAIN METHOD.
+	/*=========================================**/
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frame();
+				} catch (Exception e) {
+					System.err.println("ERROR: Unable to run Epic of Den.");
+					e.printStackTrace();
+					System.exit(1);
+				}
+			}
+		});		
+	}
+	
+	public static void frame() {
+		// Draw main frame.
+		JFrame frm = new JFrame();
+		DenPanel dp = new DenPanel();
+							
+		frm.setTitle(DenConstants.TITLE);
+		frm.add(dp);
+							
+		// Set frame settings.
+		frm.setSize(DenConstants.WIDTH, DenConstants.HEIGHT);
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frm.setLocationRelativeTo(null);
+		frm.setVisible(true);
+		frm.setResizable(false);	
+	}
+
+}
