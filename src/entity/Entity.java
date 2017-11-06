30d23198ce510124e734bc854acdea3f0c70980a
Nico Poblete <nlypoblet@gmail.com>
2017-11-05 23:58:08 -0800

    Removed action states

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.awt.image.*;
 
 import data.ImageLoader;
-import den.DenConstants;
+import main.Constants;
 
 import map.*;
 
@@ -13,37 +13,44 @@ public abstract class Entity {
 	 * CONSTANTS.
 	/**==================================*/
 	/** DIRECTIONAL ENUM CONSTANTS.**/
-	public enum Direction { UP = 0, LEFT = 1, RIGHT = 2, DOWN = 3; }
+	public enum Direction { UP, LEFT, RIGHT, DOWN };
 	
 	/** TILEMAP. **/
 	protected TileMap _map;
 	
+	/** DIRECTION **/
+	protected Direction _direction;
+	
 	/** POSITION AND VELOCITY **/
-	protected PhysicsComp _physicsComp;
-	protected double x, y; 
-	protected double dx, dy;
-	protected int tilex, tiley;
+	protected double _x, _y;
+	protected double _vel;
+	
+	/** PHYSICS COMPONENT. **/
+	protected PhysicsComp _physics;
+	
+	/** ANIMATION COMPONENT. **/
+	protected AnimationComp _animation;
 	
 	/** SIZE **/
-	protected int width, height;
-	
-	/** COLLISION BOX SIZE **/
-	protected int boxw, boxh;
+	protected int _width, _height;
 	
 	/** MOVEMENT **/
-	protected boolean up, down, left, right;
-	
-	/** ACTIONS **/
-	protected boolean moving, flashing;
+	protected boolean _up, _down, _left, _right;
 	
 	/** SPEED **/
-	protected boolean walkSpeed, runSpeed; // Aka min & max move speed
+	protected double walkSpeed, runSpeed; // Aka min & max move speed
 	protected double fallSpeed, maxFallSpeed;
 	protected double jumpSpeed, maxJumpSpeed;
 	
-	// ANIMATION OBJECT AND TOGGLE.
-	protected AnimationComp _animationComp;
-	//protected boolean _animated;
+	/**==========================
+	// CONSTRUCTOR. (for entities which won't need a map)
+	//===========================**/
+	public Entity() {
+		_map = null;
+		
+		_physics = new PhysicsComp();
+		_animation = new AnimationComp();
+	}
 	
 	/**==========================
 	// CONSTRUCTOR(tileMap).
@@ -51,26 +58,13 @@ public abstract class Entity {
 	public Entity(TileMap m) {
 		_map = m;
 		
-		_physicsComp = new PhysicsComp();
-		_animationComp = new AnimationComp();
-		
-		jumpSpeed = 2;
-		maxJumpSpeed = 5;
-		
-		fallSpeed = 2;
-		maxFallSpeed = 10;
-		
-		jumping = false;
-		falling = true;
-
-		//_animated = false;
-		
-		direction = Direction.RIGHT;
+		_physics = new PhysicsComp(_width, _height);
+		_animation = new AnimationComp();
 	}
 	
-	public void checkMapCollision() {
-		tilex = (int)(x / _map.tileSize());
-		tiley = (int)(y / _map.tileSize());
+	/*public void checkMapCollision() {
+		tilex = (int)(_x / _map.tileSize());
+		tiley = (int)(_y / _map.tileSize());
 		
 		checkTiles(tilex, tiley);
 	}
@@ -82,110 +76,89 @@ public abstract class Entity {
 		boolean rc = (tx + 1) > _map.tileSize() ? false : _map.isSolid(ty,tx + 1);
 		boolean bc = (ty + 1) > _map.tileSize() ? false : _map.isSolid(ty + 1,tx);
 		
-		if (tc) { _animationComp.setJumping(false); }
+		if (tc) { _animation.setJumping(false); }
 		if (lc) { setLeft(false); }
 		if (rc) { setRight(false); }
-		if (bc) { setFalling(false); }
-	}
+		if (bc) { _animation.setFalling(false); }
+	}*/
 	
 	/**===============================
 	/* SET METHODS.
 	/*=================================**/
-	public void setLeft(boolean l) { left = l; }
+	public void setLeft(boolean l) { _left = l; _direction = Direction.LEFT; }
 	
-	public void setRight(boolean r) { right = r; }
+	public void setRight(boolean r) { _right = r; _direction = Direction.RIGHT; }
 	
-	public void setUp(boolean u) { up = u; }
+	public void setUp(boolean u) { _up = u; _direction = Direction.UP; }
 	
-	public void setDown(boolean d) { down = d; }
-	
-	public void flash() { 
-		if (flashing) return; 
-		flashing = true; 
-	}
-	
-	//public void setAnimation(int anim) { /* Let subclasses deal w/ this. */ }
-	
-	//public void setAnimated(boolean anim) { _animated = anim; }
-	
-	public void setdx(double dx) { this.dx = dx; }
-	
-	public void setdy(double dy) { this.dy = dy; }
+	public void setDown(boolean d) { _down = d; _direction = Direction.DOWN; }
 	
 	/**============================
 	/* setLocation(x,y)
 	/*=============================**/
 	public void setLocation(double x, double y) {
-		this.x = x; 
-		this.y = y;
+		_x = x; 
+		_y = y;
 	}
 	
 	public void setLocation(int x, int y) {
-		this.x = (double)x; 
-		this.y = (double)y;
+		_x = (double)x; 
+		_y = (double)y;
+	}
+	
+	/**============================
+	 * setVelocity(v)
+	/*=============================**/
+	public void setVelocity(double v) { _vel = v; }
+	
+	public void setVelocity(int v) { _vel = (double)v; }
+	
+	/**============================
+	 * setSize(w,h)
+	/*=============================**/
+	public void setSize(int w, int h) {
+		_width = w;
+		_height = h;
 	}
 	
 	/**============================
 	 * setTileLocation(x,y)
 	/*=============================**/
-	public void setTileLocation(int x, int y) {
+	/*public void setTileLocation(int x, int y) {
 		int lx = x * _map.tileSize();
 		int ly = y * _map.tileSize();
 		setLocation(lx,ly);
-	}
-	
-	/**============================
-	/* setVelocity(dx,dy)
-	/*=============================**/
-	public void setVelocity(double dx, double dy) { 
-		this.dx = dx; 
-		this.dy = dy; 
-	}
+	}*/
 	
 	/**========================
 	/* GET METHODS.
 	/*======================**/
-	public AnimationComp animationComp() { return _animationComp; }
+	public PhysicsComp physics() { return _physics; }
 	
-	public double getx() { return x; }
+	public AnimationComp animation() { return _animation; }
 	
-	public double gety() { return y; }
+	public double x() { return _x; }
 	
-	public double getdx() { return dx; }
+	public double y() { return _y; }
 	
-	public double getdy() { return dy; }
+	public double velocity() { return _vel; }
 	
-	public int getWidth() { return width; }
+	public int width() { return _width; }
 	
-	public int getHeight() { return height; }
+	public int height() { return _height; }
+
+	//public boolean isAnimated() { return _animated; }
 	
-	public boolean isFlashing() { return flashing; }
-	
-	public Rectangle getBounds() { return new Rectangle((int)x, (int)y, boxw, boxh); }
-	
-	public boolean intersects(Entity entity) {
-		Rectangle r1 = getBounds();
-		Rectangle r2 = entity.getBounds();
-		return r1.intersects(r2);
+	/**=============================
+	 * update()
+	/**=============================**/
+	public void update() {
+		_physics.update();
+		_animation.update();
 	}
-	
-	public boolean intersects(Rectangle r) {
-		Rectangle r1 = getBounds();
-		return r1.intersects(r);
-	}
-	
-	public boolean contains(Entity entity) {
-		Rectangle r1 = getBounds();
-		Rectangle r2 = entity.getBounds();
-		return r1.contains(r2);
-	}
-	
-	public boolean isAnimated() { return _animated; }
 	
 	/**=============================
 	/* ENTITY ABSTRACT METHODS.
 	/**==============================**/
-	public abstract void update();
-	
 	public abstract void draw(Graphics g);
 }
